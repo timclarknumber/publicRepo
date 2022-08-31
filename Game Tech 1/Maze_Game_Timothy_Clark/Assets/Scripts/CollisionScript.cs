f381fafa2c9a044bddaf8dddd0aa57e77bb453e3
tim clark <timothy.clark01@mymail.champlain.edu>
2022-08-31 17:34:04 -0400

    Added functionality to prev empty script CollisionScript.cs, now puts messages in log when player collides with wall, is colliding with wall, and when it stop colliding with wall.

--- Game Tech 1/Maze_Game_Timothy_Clark/Assets/Scripts/CollisionScript.cs
+++ Game Tech 1/Maze_Game_Timothy_Clark/Assets/Scripts/CollisionScript.cs
@@ -4,6 +4,20 @@ using UnityEngine;
 
 public class CollisionScript : MonoBehaviour
 {
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log ("OnCollisionEnter2D");
+    }
+    
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        Debug.Log ("OnCollisionExit2D");
+    }
+
+    void OnCollisionStay2D(Collision2D collision)
+    {
+        Debug.Log ("OnCollisionStay2D");
+    }
     // Start is called before the first frame update
     void Start()
     {
