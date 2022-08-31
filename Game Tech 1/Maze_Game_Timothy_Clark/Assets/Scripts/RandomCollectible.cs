9d7c439531a73b60a25a56afbb5f890d4d61d585
tim clark <timothy.clark01@mymail.champlain.edu>
2022-08-31 19:48:09 -0400

    Edited RandomCollectible.cs to tutorial specifications, tested, it worked.

--- Game Tech 1/Maze_Game_Timothy_Clark/Assets/Scripts/RandomCollectible.cs
+++ Game Tech 1/Maze_Game_Timothy_Clark/Assets/Scripts/RandomCollectible.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class RandomCollectible : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    void OnTriggerEnter2D(Collider2D collider)
     {
-        
+        RepositionCollectible();
+    }
+    
+    void RepositionCollectible()
+    {
+        float newX = Random.Range (-5, 5);
+        float newY = Random.Range (-3, 3);
+        Vector2 newPos = new Vector2 (newX, newY);
+        transform.position = newPos;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
