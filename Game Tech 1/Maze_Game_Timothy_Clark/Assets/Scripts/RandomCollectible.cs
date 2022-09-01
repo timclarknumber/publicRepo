f357f24b7f8aaf9b8bdfc0f4b35ead5c7d4eef3d
tim clark <timothy.clark01@mymail.champlain.edu>
2022-08-31 20:11:41 -0400

    Modified ScoreScript.cs and RandomCollectible.cs to tutorial specifications, tested, it works!

--- Game Tech 1/Maze_Game_Timothy_Clark/Assets/Scripts/RandomCollectible.cs
+++ Game Tech 1/Maze_Game_Timothy_Clark/Assets/Scripts/RandomCollectible.cs
@@ -9,13 +9,14 @@ public class RandomCollectible : MonoBehaviour
     {
         RepositionCollectible();
     }
-    
+
     void RepositionCollectible()
     {
         float newX = Random.Range (-5, 5);
         float newY = Random.Range (-3, 3);
         Vector2 newPos = new Vector2 (newX, newY);
         transform.position = newPos;
+        GameObject.Find ("Score_Canvas").GetComponent<ScoreScript> ().AddScore ();
     }
 
 }
--- Game Tech 1/Maze_Game_Timothy_Clark/Assets/Scripts/ScoreScript.cs
+++ Game Tech 1/Maze_Game_Timothy_Clark/Assets/Scripts/ScoreScript.cs
@@ -1,18 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;//See below
 
 public class ScoreScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public TextMeshProUGUI scoreText; //Using TextMeshProUGUI instead of just public Text because the Text type is now legacy
+    //I found out I had to add using TMPro; and change my Text to TextMeshProUGUI from this thread: https://www.reddit.com/r/Unity3D/comments/fdtuf7/how_can_i_reference_textmeshpro_text_in/
+    int score = 0;
 
-    // Update is called once per frame
-    void Update()
+    public void AddScore()
     {
-        
+        score++;
+        scoreText.text = score.ToString ();
     }
 }
