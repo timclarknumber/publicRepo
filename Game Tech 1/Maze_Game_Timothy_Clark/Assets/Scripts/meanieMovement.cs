4a263295b8137c76be86e5b8ca3753dd1a4114ed
tim clark <timothy.clark01@mymail.champlain.edu>
2022-09-06 19:47:09 -0400

    Reoranized meanieMovement.cs so that moving vertically and horizontally are broken up into separate functions, should help readability.

--- Game Tech 1/Maze_Game_Timothy_Clark/Assets/Scripts/meanieMovement.cs
+++ Game Tech 1/Maze_Game_Timothy_Clark/Assets/Scripts/meanieMovement.cs
@@ -13,66 +13,77 @@ public class meanieMovement : MonoBehaviour
     bool forwardOrBackward =  true; //whether the meanie is currently going forward or backward, true is forward & vv, forward for H is right & vv, forward for V is up & vv
     Vector3 newPosition = new Vector3(0.0f, 0.0f, 0.0f);
 
+    void moveTheMeanieV()
+    {
+        if (forwardOrBackward) //meanie is going forward (up)
+        {
+            if (newPosition.y < yExtentUp) //meanie is not yet past farthest extent upward
+            {
+                newPosition.y += meanieSpeed; //meanie moves up
+                Debug.Log ("meanie is moving up");
+            } else //meanie is past farthest extent upward
+            {
+                forwardOrBackward = false; //meanie will now go backwards (down)
+            }
+        } else //meanie is going backward (down)
+        {
+            if (newPosition.y > yExtentDown) //meanie is not yet past farthest extent downard
+            {
+                newPosition.y -= meanieSpeed; //meanie moves down
+                Debug.Log ("meanie is moving down");
+            } else //meanie is past farthest extent downward;
+            {
+                forwardOrBackward = true; //meanie will now go forwards (up)
+            }
+        }
+    }
+
+    void moveTheMeanieH()
+    {
+        if (forwardOrBackward) //meanie is going forward (right)
+        {
+            if (newPosition.x < xExtentRight) //meanie is not yet past farthest extent to the right
+            {
+                newPosition.x += meanieSpeed; //meanie moves to the right
+                Debug.Log ("meanie is moving right");
+            } else //meanie is past farthest extent to the right
+            {
+                forwardOrBackward = false; //meanie will now go backwards (left)
+            }
+        } else //meanie is going backward (left)
+        {
+            if (newPosition.x > xExtentLeft) //meanie is not yet past farthest extent downard
+            {
+                newPosition.x -= meanieSpeed; //meanie moves left
+                Debug.Log ("meanie is moving left");
+            } else //meanie is past farthest extent to the left;
+            {
+                forwardOrBackward = true; //meanie will now go forwards (right)
+            }
+        }
+    }
+
     void moveTheMeanie()
     {
         if (meanieHorV) //meanie is going Vertically
         {
-            if (forwardOrBackward) //meanie is going forward (up)
-            {
-                if (newPosition.y < yExtentUp) //meanie is not yet past farthest extent upward
-                {
-                    newPosition.y += meanieSpeed; //meanie moves up
-                    Debug.Log ("meanie is moving up");
-                } else //meanie is past farthest extent upward
-                {
-                    forwardOrBackward = false; //meanie will now go backwards (down)
-                }
-            } else //meanie is going backward (down)
-            {
-                if (newPosition.y > yExtentDown) //meanie is not yet past farthest extent downard
-                {
-                    newPosition.y -= meanieSpeed; //meanie moves down
-                    Debug.Log ("meanie is moving down");
-                } else //meanie is past farthest extent downward;
-                {
-                    forwardOrBackward = true; //meanie will now go forwards (up)
-                }
-            }
-        } else { //meanie is going Horizontally
-            if (forwardOrBackward) //meanie is going forward (right)
-            {
-                if (newPosition.x < xExtentRight) //meanie is not yet past farthest extent to the right
-                {
-                    newPosition.x += meanieSpeed; //meanie moves to the right
-                    Debug.Log ("meanie is moving right");
-                } else //meanie is past farthest extent to the right
-                {
-                    forwardOrBackward = false; //meanie will now go backwards (left)
-                }
-            } else //meanie is going backward (left)
-            {
-                if (newPosition.x > xExtentLeft) //meanie is not yet past farthest extent downard
-                {
-                    newPosition.x -= meanieSpeed; //meanie moves left
-                    Debug.Log ("meanie is moving left");
-                } else //meanie is past farthest extent to the left;
-                {
-                    forwardOrBackward = true; //meanie will now go forwards (right)
-                }
-            }
+            moveTheMeanieV();
+        } else  //meanie is going Horizontally
+        {
+            moveTheMeanieH();
         }
-        transform.localPosition = newPosition;
+        transform.localPosition = newPosition; //moves the meanie
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        newPosition = transform.localPosition;
+        newPosition = transform.localPosition; //set the variable used to track meanie's pos to the position of meanie at game start
     }
 
     // Update is called once per frame
     void Update()
     {
-        moveTheMeanie();
+        moveTheMeanie(); //moves the meanie every frame
     }
 }
