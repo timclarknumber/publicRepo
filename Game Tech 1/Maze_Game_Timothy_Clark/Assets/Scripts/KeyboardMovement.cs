c242774b229eb2fb970f469916f38addde481e6c
tim clark <timothy.clark01@mymail.champlain.edu>
2022-09-06 18:33:23 -0400

    Created a third scene by duplicating 'Scene_2_Basic_Collectible', in this new scene (Scene_3_Basic_Collectible), the camera is a child of the Player, the Player object's z rotation is locked, the camera is larger, there is at least one more wall, the original wall is resized, and the camera is centered on the player.
    Revamped the script 'Keyboard Movement' to stop the player when there is no key pressed while still supporting movement in 8 directions utilizing rb.velocity (replaces rb.AddForce method)
    
    Note: There is still a problem with input delay. Try to fix this at some point, but for now movement is more functional than it was, if a bit uncomfortable.

--- Game Tech 1/Maze_Game_Timothy_Clark/Assets/Scripts/KeyboardMovement.cs
+++ Game Tech 1/Maze_Game_Timothy_Clark/Assets/Scripts/KeyboardMovement.cs
@@ -6,6 +6,7 @@ public class KeyboardMovement : MonoBehaviour
 {
     float xMove = 0f;
     float yMove = 0f;
+    Vector2 playerVelocity = new Vector2(0.0f, 0.0f);
     Rigidbody2D rb;
     public float speed = 10f;
     // Start is called before the first frame update
@@ -22,13 +23,46 @@ public class KeyboardMovement : MonoBehaviour
 
     void MovePlayer()
     {
-        rb.AddForce (new Vector2 (xMove, yMove) * speed);
+        //rb.AddForce (new Vector2 (xMove, yMove) * speed);
+        if (yMove > 0)
+        {
+            if (playerVelocity.y < speed)
+            {
+                playerVelocity += new Vector2(0.0f, speed);
+            }
+        } else if (yMove < 0)
+        {
+            if (playerVelocity.y > -speed)
+            {
+                playerVelocity -= new Vector2(0.0f, speed);
+            }
+        } else
+        {
+            playerVelocity.y = 0.0f;
+        }
+
+        if (xMove > 0)
+        {
+            if (playerVelocity.x < speed)
+            {
+                playerVelocity += new Vector2(speed, 0.0f);
+            }
+        } else if (xMove < 0)
+        {
+            if (playerVelocity.x > -speed)
+            {
+                playerVelocity -= new Vector2(speed, 0.0f);
+            }
+        } else
+        {
+            playerVelocity.x = 0.0f;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         CheckInput();
         MovePlayer();
+        rb.velocity = playerVelocity;
     }
 }
