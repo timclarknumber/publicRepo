3286af2048824b077f54447251c6e27e6979e22e
clark <timothy.clark01@mymail.champlain.edu>
2022-09-13 00:30:12 -0400

    Bug Fix/Functionality addition, edited scripts, when switching from the end scene to the start scene, the score canvas now gets destroyed, so playing the game multiple times doesnt produce multiple instances of the score canvas.

--- Game Tech 1/Maze_Game_Timothy_Clark/Assets/Scripts/SceneSwitch.cs
+++ Game Tech 1/Maze_Game_Timothy_Clark/Assets/Scripts/SceneSwitch.cs
@@ -8,10 +8,17 @@ using UnityEngine.SceneManagement;
 public class SceneSwitch : MonoBehaviour
 {
     public string sceneName;
+    public GameObject canvas;
     // Update is called once per frame
     void Update()
     {
+        canvas = GameObject.Find("Score_Canvas");
         CheckStart(); //if the player clicks, change the scene
+    }
+
+    void Start()
+    {
+
     }
 
     void CheckStart()
@@ -19,6 +26,7 @@ public class SceneSwitch : MonoBehaviour
         {
             if (Input.GetMouseButtonDown (0)) //check if the player (ie the user, not the object) has clicked
             {
+                Destroy(canvas);
                 //Change the scene name depending on which script you are on.
                 //Note that this will require that each SceneManager has a string.
                 //typed into the Unity Inspector sceneName.
