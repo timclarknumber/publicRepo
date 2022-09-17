e2e8fc99d521edb18dc6f7a4a6a81d5e4a6efd32
tim clark <timothy.clark01@mymail.champlain.edu>
2022-09-17 14:06:15 -0400

    Changed script functionality so now the lasers will remove themselves past a certain distance, also changed the script a little bit so more things can be changed in editor.

--- Game Tech 1/VerticalShooter/Assets/Scripts/MoveLaser.cs
+++ Game Tech 1/VerticalShooter/Assets/Scripts/MoveLaser.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class MoveLaser : MonoBehaviour
 {
+    public float laserMovesAt = 500;
+    public float laserLife = 60f;
+    float lifeCount = 0f;
     // Start is called before the first frame update
     void Start()
     {
-        GetComponent<Rigidbody2D> ().AddForce (transform.up * 500);
+        GetComponent<Rigidbody2D> ().AddForce (transform.up * laserMovesAt);
+    }
+
+    void CheckLife()
+    {
+        lifeCount++;
+        if (lifeCount == laserLife)
+        {
+            Destroy(gameObject);
+        }    
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        CheckLife();
     }
 }
