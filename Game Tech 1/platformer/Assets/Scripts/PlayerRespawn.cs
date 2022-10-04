9f2717294932313a544fb1e15c75bec5710d164e
clark <timothy.clark01@mymail.champlain.edu>
2022-10-03 21:38:30 -0400

    Finished Video Tutorials

---
+++ Game Tech 1/platformer/Assets/Scripts/PlayerRespawn.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerRespawn : MonoBehaviour
+{
+
+    [SerializeField] private Transform respawn;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Hazard"))
+        {
+            //Respawn
+            transform.position = respawn.transform.position;
+        }
+        else if (other.gameObject.CompareTag("Checkpoint"))
+        {
+            //Respawn
+            respawn = other.transform;
+        }
+    }
+}
