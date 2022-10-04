4077c0962e92c1c6907fa6ce926bce3afd368b13
clark <timothy.clark01@mymail.champlain.edu>
2022-10-03 20:05:28 -0400

    Modified script added in last commit, added an animation.

--- Game Tech 1/platformer/Assets/Scripts/PlayerAnimation.cs
+++ Game Tech 1/platformer/Assets/Scripts/PlayerAnimation.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class PlayerAnimation : MonoBehaviour
 {
+    [SerializeField] private Rigidbody2D rb;
+    private bool _facingRight = true;
     // Start is called before the first frame update
-    void Start()
+    private void Awake()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (_facingRight && rb.velocity.x < -0.1)
+        {
+            Flip();
+        } else if (!_facingRight && rb.velocity.x > 0.1)
+        {
+            Flip();
+        }
+    }
+
+    private void Flip()
+    {
+        _facingRight = !_facingRight;
+        transform.localScale = new Vector3(transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
     }
 }
