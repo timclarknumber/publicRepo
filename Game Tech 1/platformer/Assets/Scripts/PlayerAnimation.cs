bc2f04ff01b414f98e212483943999f1151f1c23
clark <timothy.clark01@mymail.champlain.edu>
2022-10-17 23:05:23 -0400

    Fixed everything again. It all works now. Had to pull because I broke everything. Also, the enemy now gets hit and reacts and does stuff and it's just great.

--- Game Tech 1/platformer/Assets/Scripts/PlayerAnimation.cs
+++ Game Tech 1/platformer/Assets/Scripts/PlayerAnimation.cs
@@ -10,6 +10,7 @@ public class PlayerAnimation : MonoBehaviour
     [SerializeField] private PlayerRespawn playerRespawn;
     [SerializeField] private SpriteRenderer legColor;
     [SerializeField] private SpriteRenderer handColor;
+    [SerializeField] private Transform eyesOpen;
     public bool facingRight = true;
     private Color _handColorActual;
     private Color _legColorActual;
@@ -25,6 +26,7 @@ public class PlayerAnimation : MonoBehaviour
         {
             if (_handColorActual.r == 1f && _legColorActual.b == 1f) //are the player's hands red and legs blue? If so, that means they aren't attacking (animation changes color of these limbs when attacking)
             { //if they aren't attacking, they are allowed to get hit. This effectively means the player has I-frames when attacking which is just fine.
+                Debug.Log("Uh");
                 animator.SetBool("PlayerGotHit", true); //the player just got hit! Do the animation.
             }
         }
@@ -32,7 +34,7 @@ public class PlayerAnimation : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (transform.localScale.x < 0.1)
+        if (eyesOpen.localScale.y <= 0.05)
         {
             animator.SetBool("PlayerGotHit", false);
             playerRespawn.RespawnThePlayer();
