05fef12d49bc96aedae8a6be917eca69c4b2d302
tim clark <timothy.clark01@mymail.champlain.edu>
2022-12-16 06:24:37 -0500

    (P) Improved descriptor text

--- Principles of Design/EEE motion/Assets/Scripts/DescriptorText.cs
+++ Principles of Design/EEE motion/Assets/Scripts/DescriptorText.cs
@@ -37,15 +37,19 @@ public class DescriptorText : MonoBehaviour
                 } else if (numberICareAbout == -1)
                 {
                     textISay = "A bit sad";
+                    thisText.color = new Color32(0, 234, 255, 255);
                 } else if (numberICareAbout == 0)
                 {
                     textISay = "Apathetic to sadness and joy";
+                    thisText.color = new Color32(0, 0, 0, 255);
                 } else if (numberICareAbout == 1)
                 {
                     textISay = "Cheery";
+                    thisText.color = new Color32(0, 255, 101, 255);
                 } else if (numberICareAbout == 2)
                 {
                     textISay = "Joyous!";
+                    thisText.color = new Color32(24, 255, 0, 255);
                 }
             } else if (whichAmI == 2)
             { 
@@ -53,18 +57,23 @@ public class DescriptorText : MonoBehaviour
                 if (numberICareAbout == -2)
                 {
                     textISay = "Constantly enraged";
+                    thisText.color = new Color32(85, 0, 0, 255);
                 } else if (numberICareAbout == -1)
                 {
                     textISay = "Irratable";
+                    thisText.color = new Color32(200, 0, 0, 255);
                 } else if (numberICareAbout == 0)
                 {
                     textISay = "Apathetic to anger and love";
+                    thisText.color = new Color32(0, 0, 0, 255);
                 } else if (numberICareAbout == 1)
                 {
                     textISay = "Caring";
+                    thisText.color = new Color32(255, 0, 75, 255);
                 } else if (numberICareAbout == 2)
                 {
                     textISay = "Loving";
+                    thisText.color = new Color32(255, 0, 180, 255);
                 }
             } else if (whichAmI == 3)
             {
@@ -72,18 +81,23 @@ public class DescriptorText : MonoBehaviour
                 if (numberICareAbout == -2)
                 {
                     textISay = "Totally alone";
+                    thisText.color = new Color32(109, 0, 255, 255);
                 } else if (numberICareAbout == -1)
                 {
                     textISay = "Awkward";
+                    thisText.color = new Color32(175, 0, 255, 255);
                 } else if (numberICareAbout == 0)
                 {
                     textISay = "Apathetic to contact with others";
+                    thisText.color = new Color32(0, 0, 0, 255);
                 } else if (numberICareAbout == 1)
                 {
                     textISay = "A socialite";
+                    thisText.color = new Color32(206, 0, 255, 255);
                 } else if (numberICareAbout == 2)
                 {
                     textISay = "Always talking";
+                    thisText.color = new Color32(151, 82, 161, 255);
                 }
             } else if (whichAmI == 4)
             {
@@ -91,18 +105,23 @@ public class DescriptorText : MonoBehaviour
                 if (numberICareAbout == -2)
                 {
                     textISay = "Terrified";
+                    thisText.color = new Color32(0, 99, 22, 255);
                 } else if (numberICareAbout == -1)
                 {
                     textISay = "Easy to frighten";
+                    thisText.color = new Color32(0, 166, 37, 255);
                 } else if (numberICareAbout == 0)
                 {
                     textISay = "Apathetic to fear and confidence";
+                    thisText.color = new Color32(0, 0, 0, 255);
                 } else if (numberICareAbout == 1)
                 {
                     textISay = "Confident";
+                    thisText.color = new Color32(255, 229, 0, 255);
                 } else if (numberICareAbout == 2)
                 {
                     textISay = "Smug";
+                    thisText.color = new Color32(255, 178, 0, 255);
                 }
             }
         }
